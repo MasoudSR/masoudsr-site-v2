b1e04e44c3dfa28add9fb1fbe937f9109ed4e96e
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-19 12:59:24 +0330

    using json data for skills

--- src/components/modules/Skill.jsx
+++ src/components/modules/Skill.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
 
-function Skill({ skillName , fileName }) {
+function Skill({ title, imgURL }) {
     return (
         <div className='w-16 h-16 p-2 rounded-xl shadow-2xl shadow-primary flex justify-center items-center'>
-            <Image src={`/images/skills/${fileName}`} width={40} height={40} alt={skillName} className='drop-shadow-2xl'/>
+            <Image src={imgURL} width={40} height={40} alt={title} className='drop-shadow-2xl' />
         </div>
     )
 }
