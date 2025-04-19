19b20182c7d86df363f9cd7725f14d1f7e372482
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-19 14:54:52 +0330

    about component created

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='text-center py-16'>
+            <h2 className='text-2xl'>
+                AmirMasoud SepehrRad
+            </h2>
+            <h3 className='text-xl'>
+                MSc in Software Engineering
+            </h3>
+        </div>
+    )
+}
+
+export default About
