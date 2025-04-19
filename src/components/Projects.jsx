a612036d257e97fdde10bdb891ad8c9a26371a3a
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-19 17:44:26 +0330

    projects component created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Project from './modules/Project'
+import projectsData from "@/data/projects.json"
 
 function Projects() {
     return (
         <div className='p-4'>
-            <h1 className='text-5xl text-center mt-16'>Projects</h1>
-            <div className='flex flex-wrap gap-4 justify-center py-24'>
-                <Project />
-                <Project />
-                <Project />
+            <h1 className='text-5xl text-center mt-16 drop-shadow-2xl text-primary drop-shadow-primary'>Projects</h1>
+            <div className='flex flex-wrap gap-6 justify-center mt-8'>
+                {projectsData.map(project => <Project key={project.title} data={project} />)}
             </div>
         </div>
     )
--- src/components/modules/Project.jsx
+++ src/components/modules/Project.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
-function Project() {
+function Project({ data: { title, subtitle, description, imgURL, link } }) {
   return (
-    <div>Project</div>
+    <div className='p-2 bg-gray-50 shadow-sm shadow-gray-300 rounded-xl w-full'>
+      <div className='border rounded-lg border-gray-200 flex justify-center bg-white p-2'>
+        <img src={imgURL} alt={title} className='h-52' />
+      </div>
+      <div className='p-3 border-b border-gray-300'>
+        <div className='flex items-baseline gap-1'>
+          <h4 className='text-xl'>{title}</h4>
+          <h5 className='text-gray-800'>{subtitle}</h5>
+        </div>
+        <p className='text-sm mt-4 mb-2'>{description}</p>
+      </div>
+      <a href={link} className='px-4 py-2 mt-2 text-center bg-primary drop-shadow-sm drop-shadow-primary block text-white rounded-lg'>Open</a>
+    </div>
   )
 }
 
