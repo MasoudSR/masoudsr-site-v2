ea70bb88e07faad7066222d8ecf92040a612830f
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-25 12:06:41 +0330

    contact improved for desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,12 @@ import contactData from "../data/contact.json"
 
 function Contact() {
     return (
-        <div className='p-4 min-h-screen bg bg-primary rounded-t-4xl' id='contact'>
-            <h1 className='text-5xl text-center mt-10 text-white'>Contact</h1>
-            <div className='flex flex-col gap-4 justify-center py-24'>
-                {contactData.map(contact => <ContactCard key={contact.title} contact={contact} />)}
+        <div className='p-4 bg-primary rounded-t-4xl lg:rounded-none mt-26 py-28' id='contact'>
+            <div className='max-w-screen-lg m-auto'>
+                <h1 className='text-5xl text-center text-white drop-shadow-2xl drop-shadow-white'>Contact</h1>
+                <div className='grid lg:grid-cols-2 gap-4 mt-24'>
+                    {contactData.map(contact => <ContactCard key={contact.title} contact={contact} />)}
+                </div>
             </div>
         </div>
     )
--- src/components/modules/ContactCard.jsx
+++ src/components/modules/ContactCard.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 
-function ContactCard({ contact: { title, description, imgURL } }) {
+function ContactCard({ contact: { title, description, imgURL, link } }) {
     return (
-        <div className='bg-white flex justify-between p-1 pl-6 rounded-full'>
+        <a href={link} className='bg-white flex justify-between p-1 pl-6 rounded-full shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0.5 active:inset-shadow-sm'>
             <div>
                 <h5 className='text-xl'>{title}</h5>
                 <p className='text-sm text-gray-600'>{description}</p>
@@ -11,7 +11,7 @@ function ContactCard({ contact: { title, description, imgURL } }) {
             <div>
                 <Image src={imgURL} alt="" width={50} height={50} />
             </div>
-        </div>
+        </a>
     )
 }
 
