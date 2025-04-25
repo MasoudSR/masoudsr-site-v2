fe550029aa1c27580ff2f97bff4e1a1f4dafe631
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-25 13:03:53 +0330

    about ui improved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,17 @@ import Skills from "@/components/Skills";
 function About() {
     return (
         <div id='about' className="pt-26 lg:pt-56">
-            <div className='flex flex-col lg:flex-row max-w-screen-lg m-auto gap-6 lg:gap-0'>
-                <div className="m-4 px-2 rounded-lg lg:w-[50%] flex flex-col justify-center items-center">
-                    <h2 className='text-5xl font-bold  text-white drop-shadow-primary drop-shadow-sm text-center'>
-                        <div className="lg:-translate-x-20">
+            <div className='flex flex-col lg:flex-row max-w-screen-lg m-auto gap-24 lg:gap-0'>
+                <div className="m-4 lg:w-[50%] flex flex-col justify-center items-center text-primary drop-shadow-primary/40 drop-shadow-2xl">
+                    <h2 className='text-5xl font-bold py-4 text-center border-y-4 lg:border-none'>
+                        <div className="lg:-translate-x-20 lg:underline underline-offset-1">
                             AmirMasoud
                         </div>
-                        <div className="lg:translate-x-20">
+                        <div className="lg:translate-x-20 lg:overline">
                             SepehrRad
                         </div>
                     </h2>
-                    <h3 className='text-xl mt-5 lg:mt-10 rounded-full drop-shadow-lg drop-shadow-primary/50 bg-primary text-white w-fit p-1 px-5'>
+                    <h3 className='text-xl py-2 lg:mt-10 text-center p-1 px-5'>
                         MSc in Software Engineering
                     </h3>
                 </div>
--- src/components/modules/Skill.jsx
+++ src/components/modules/Skill.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Skill({ title, imgURL }) {
     return (
-        <div className='w-16 h-16 p-2 rounded-xl shadow-lg shadow-primary/30 bg-white flex justify-center items-center'>
+        <div className='w-16 h-16 p-2 rounded-xl shadow-lg shadow-primary/30 bg-white flex justify-center z-10 items-center'>
             {/* <Image src={imgURL} width={40} height={40} alt={title} className='drop-shadow-2xl' /> */}
             <img src={imgURL} alt={title} className='w-10 h-10' />
         </div>
