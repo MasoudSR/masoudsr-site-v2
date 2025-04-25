37572bc11784f438d8f921c407503595bf0ffcd3
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-25 16:59:08 +0330

    navbar improved

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import contactData from "../data/contact.json"
 
 function Contact() {
     return (
-        <div className='p-4 bg-primary rounded-t-4xl lg:rounded-none mt-26 py-28' id='contact'>
+        <div className='p-4 bg-primary rounded-t-4xl lg:rounded-none mt-26 py-28 min-h-screen' id='contact'>
             <div className='max-w-screen-lg m-auto'>
                 <h1 className='text-5xl text-center text-white drop-shadow-2xl drop-shadow-white'>Contact</h1>
                 <div className='grid lg:grid-cols-2 gap-4 mt-24'>
--- src/components/modules/NavBtn.jsx
+++ src/components/modules/NavBtn.jsx
@@ -1,7 +1,15 @@
 "use client"
-import { Link } from 'react-scroll';
+import { useEffect } from 'react';
+import { Events, Link } from 'react-scroll';
 
 function NavBtn({ address, children, activePage, setActivePage }) {
+
+  useEffect(() => {
+    Events.scrollEvent.register('end', (to) => {
+      setActivePage(to)
+    });
+  }, [])
+
 
   const handleSetActive = () => {
     setActivePage(address)
@@ -10,9 +18,9 @@ function NavBtn({ address, children, activePage, setActivePage }) {
   return (
     <Link to={address}
       onSetActive={handleSetActive}
-      spy smooth duration={500} offset={address !== "contact" && -55}
+      spy smooth duration={500} offset={-80}
       spyThrottle={500}
-      className={`rounded-full  px-4 py-2 flex flex-col items-center w-[25%] transition-all duration-300 z-20 ${activePage === address ? "text-primary" : "text-white"}`}>
+      className={`rounded-full select-none cursor-pointer px-4 py-2 flex flex-col items-center w-[25%] transition-all duration-300 z-20 ${activePage === address ? "text-primary" : "text-white"}`}>
       {children}
     </Link>
   )
