0f333126f3999a427abc1adc1c47fec5c4da5101
Masoud S.R <amir.sepehr72@gmail.com>
2025-04-25 11:18:24 +0330

    about optimized for desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,23 @@ import Skills from "@/components/Skills";
 
 function About() {
     return (
-        <div className='text-center' id='about'>
-            <div className="m-4 px-2 py-8 rounded-lg -translate-y-[30%] bg-white drop-shadow-lg drop-shadow-primary/50 border border-primary">
-                <h2 className='text-3xl font-bold  text-white drop-shadow-primary drop-shadow-sm'>
-                    AmirMasoud SepehrRad
-                </h2>
-                <h3 className='text-xl'>
-                    MSc in Software Engineering
-                </h3>
+        <div id='about' className="pt-26 lg:pt-56">
+            <div className='flex flex-col lg:flex-row max-w-screen-lg m-auto gap-6 lg:gap-0'>
+                <div className="m-4 px-2 rounded-lg lg:w-[50%] flex flex-col justify-center items-center">
+                    <h2 className='text-5xl font-bold  text-white drop-shadow-primary drop-shadow-sm text-center'>
+                        <div className="lg:-translate-x-20">
+                            AmirMasoud
+                        </div>
+                        <div className="lg:translate-x-20">
+                            SepehrRad
+                        </div>
+                    </h2>
+                    <h3 className='text-xl mt-5 lg:mt-10 rounded-full drop-shadow-lg drop-shadow-primary/50 bg-primary text-white w-fit p-1 px-5'>
+                        MSc in Software Engineering
+                    </h3>
+                </div>
+                <Skills />
             </div>
-            <Skills />
         </div>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import skillsData from "@/data/skills.json"
 
 function Skills() {
     return (
-        <div className='p-4'>
+        <div className='p-4 lg:w-[50%]'>
             <h1 className='text-5xl text-center drop-shadow-2xl text-primary drop-shadow-primary'>Skills</h1>
             <div className='flex flex-wrap gap-4 justify-center mt-8'>
                 {skillsData.map(skill => <Skill key={skill.title} title={skill.title} imgURL={skill.imgURL} />)}
